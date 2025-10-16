a0bad24edcc241825778abfd790c8cde8e6abfd4
Killian Mayrhofer <killian.mayrhofer@htlwy.at>
2025-10-16 16:03:08 +0200

    threads

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Bitte Start- und Endwert als Argumente angeben!");
+            return;
+        }
+
+        int start = int.Parse(args[0]);
+        int ende = int.Parse(args[1]);
+
+        Console.WriteLine($"Primzahlen von {start} bis {ende}:");
+
+        for (int i = start; i <= ende; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ PrimeNumbers/ThreadExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+internal class Programm
+{
+    class Counter
+    {
+        private ConsoleColor color;
+
+        public Counter(ConsoleColor color)
+        {
+            this.color = color;
+        }
+
+        public void Count()
+        {
+            for (int i = 0; i < 100; i++) // kleinerer Wert zum Testen
+            {
+                lock (Console.Out) // verhindert dass sich Threads beim Schreiben überlappen
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(i);
+                }
+                Thread.Sleep(10); // Optional, um die Ausgabe besser sichtbar zu machen
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Counter counter1 = new Counter(ConsoleColor.Red);
+        Counter counter2 = new Counter(ConsoleColor.Green);
+
+        Thread thread1 = new Thread(counter1.Count);
+        Thread thread2 = new Thread(counter2.Count);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.ResetColor();
+    }
+}
