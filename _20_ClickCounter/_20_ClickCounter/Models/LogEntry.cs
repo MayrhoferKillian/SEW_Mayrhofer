cc367b6058516b97a4c23ef001e250b7cc3d681c
MayrhoferKillian <113448701+MayrhoferKillian@users.noreply.github.com>
2026-03-22 17:33:22 +0100

    ClickCounter mit fehler

---
+++ _20_ClickCounter/_20_ClickCounter/Models/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20_ClickCounter.Models
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ _20_ClickCounter/_20_ClickCounter/ViewModels/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using _20_ClickCounter.Helpers;
+using _20_ClickCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace _20_ClickCounter.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _currentId = 0;
+
+        public ObservableCollection<LogEntry> LogEntries { get; set; }
+
+        public ICommand LogClickCommand { get; }
+        public ICommand ClearListCommand{ get; }
+
+        public int TotalClickCount => LogEntries.Count;
+
+        public MainViewModel()
+        {
+            LogEntries = new ObservableCollection<LogEntry>();
+            LogEntries.CollectionChanged += (s, e) => OnPropertyChanged(nameof(TotalClickCount));
+
+            LogClickCommand = new RelayCommand(ExecuteLogClick);
+            ClearListCommand = new RelayCommand(ExecuteClearList, CanExecuteClearList);
+        }
+
+        private void ExecuteLogClick(object parameter)
+        {
+            _currentId++;
+            LogEntries.Add(new LogEntry
+            {
+                Id = _currentId,
+                Timestamp = DateTime.Now
+            });
+        }
+
+        private void ExecuteClearList(object parameter) 
+        {
+            LogEntries.Clear();
+            _currentId = 0;
+        }
+
+        private bool CanExecuteClearList(object parameter)
+        {
+            return LogEntries.Count > 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
